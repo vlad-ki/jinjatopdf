e3b7b37386dbe2edfc2b95aa6ecbe2bbc00cac2f
vlad-ki <inweb24.vk@gmail.com>
2017-06-26 19:56:05 +0300

    remove comments

--- setup.py
+++ setup.py
@@ -6,19 +6,7 @@ setup(
     author="Vladimir Kipiani",
     author_email="inweb24.vk@gmail.com",
     packages=find_packages(),
-    # install_requires=[
-    #     'selenium>=3.0.2',
-    #     'behave',
-    #     'requests'
-    # ],
-    # package_data={
-    #     'tc_selenium': [
-    #         'features/*.feature',
-    #         'drivers/chromedriver',
-    #         'drivers/firefoxdriver',
-    #         'drivers/darwin/chrome'
-    #     ],
-    # },
+
     entry_points={
         'console_scripts':
             ['jinjatopdf = jinjatopdf.app:main']
